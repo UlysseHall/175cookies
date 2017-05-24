808e21cd15fd6ea50ff4e3e07ec78072a1f37355
UlysseHall <ulysse.hall@hetic.net>
2017-05-24 17:48:19 +0200

    footer fini

--- footer.php
+++ footer.php
@@ -1,4 +1,13 @@
         </div>
+        
+        <footer>
+            <ul>
+                <li><a href="#">Mentions légales</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Nous rejoindre</a></li>
+            </ul>
+        </footer>
+        
         <script src="vendor/jquery/jquery.js"></script>
         <script src="public/js/loadProduct.js"></script>
     </body>
